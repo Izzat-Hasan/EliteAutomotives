544e32c70a8ebfbe7a8902ed4d8b1ccb7c8704e8
Izzat-Hasan <izzathasan03@gmail.com>
2021-12-16 18:12:50 -0600

    final

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let search = document.querySelector('.search-box');
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
-let menu = document.querySelector('.navbar');
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
