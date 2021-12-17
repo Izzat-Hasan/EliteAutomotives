c28b047d431cc271dc93cf0e7c0c6deee7aeb3f9
Izzat-Hasan <izzathasan03@gmail.com>
2021-12-16 18:31:09 -0600

    email

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@ if(isset($_POST['submit'])){
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo="izzathasan@lewisu.edu";
+  $mailTo="izzathasan03@gmail.com";
   $headers = "From: ".$mailFrom;
   $txt ="you have received an e-mail from".$name.".\n\n".$message;
   mail($mailTo,$subject,$txt,$headers);
